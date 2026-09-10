1bc150c19a7891b66898b35db3989e8bb7e3935f
bhavyyy <bhavy2866sharma@gmail.com>
2026-09-10 23:56:43 +0530

    feat: initialize CampusFlow

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+int choice;
+    
+cout << "==========================="<< endl;
+cout <<  "      CAMPUSFLOW              "<< endl;
+cout << "   Campus Management System "<< endl;
+cout << "==========================="<< endl;
+cout << "1.Student Management" << endl;
+cout << "2.Course Management" << endl;
+cout << "3.Library" << endl;
+cout << "4.Timetable"<< endl;
+cout << "5.Exit"<< endl;
+cout << "Enter Your Choice : " ;
+cin >> choice;
+
+}
